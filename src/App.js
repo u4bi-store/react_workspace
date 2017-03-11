f37ed841820b424ec6327a5fceeef4968c6d10d6
u4bi <u4bi@u4bi.com>
2017-03-12 02:06:20 +0900

    리엑트 테스트 - css 바인딩 과정 서베이 - jsx  바인딩 과정 조사

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './u4bi.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="Main">
+        <div className="Main-header">
+          <img src={logo} className="Main-logo" alt="logo" />
+          <h2>U4BI 리엑트</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p className="Main-intro">
+          리엑트 디폴트 페이지 <code>src/App.js</code>입니다.
         </p>
       </div>
     );
