49531af21b9810d2912b6190c1d9c909a471dea1
u4bi <u4bi@u4bi.com>
2017-03-12 10:40:24 +0900

    웹팩 babel 변경내용 수정
    - 버전업되면서 변경된 사항이라고 한다.
    - babel - babel-loader

---
+++ react-basic/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'; //  require('..') 의 역할
+ 
+class App extends React.Component {
+    render(){
+ 
+        return (
+                <h1>Hello React Skeleton</h1>
+        );
+    }
+}
+ 
+export default App; // module.export = App 와 같습니다.
+
+// 모두 es6에서 도입된 키워드
---
+++ react-basic/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+ 
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
+
+// App 컴포넌트를 불러와서 root element
