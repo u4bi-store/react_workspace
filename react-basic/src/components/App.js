6f5f94e7adc226c81271a2e92c4f8865d71352fc
u4bi <u4bi@u4bi.com>
2017-03-12 10:45:50 +0900

    jsx 문법 베이직

--- react-basic/src/components/App.js
+++ react-basic/src/components/App.js
@@ -1,11 +1,20 @@
 import React from 'react'; //  require('..') 의 역할
  
-class App extends React.Component {
-    render(){
+class App extends React.Component { // class 개념 역시 ES6 에 새로 도입된 요소
+                                    // 모든 Component는 React.Component 를 상속
+    
+    render(){ // render() 메소드에서는 컴포넌트에 렌더링 될 데이타를 정의
  
-        return (
-                <h1>Hello React Skeleton</h1>
+        return ( // 이 부분이 JSX의 가장 중요한 부분입니다.
+                 // 자바스크립트에서 html 태그를 반환
+                 // 따옴표같은건 없다.
+                 // React JSX 는 XML-like Syntax 를 native Javascript 변환해준다.
+                 // ” ” 로 감싸지 않는 점 주의
+                <h1>Hello U4bi</h1>
         );
+        
+        // 이전에는 개발자들이 .jsx 확장자를 사용하였지만
+        // 요즘은 .js 를 사용하는 추세
     }
 }
  
